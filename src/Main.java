46b36ac4454d2a4a5865cdad54998ed87f330c42
JosueHerrera <joeherrera5th@gmail.com>
2017-11-12 17:04:59 -0800

    Created Outputfile and ability to write to it.

--- src/Main.java
+++ src/Main.java
@@ -11,11 +11,14 @@ public class Main {
     public static void main(String[] args) {
         String testFileName = args[0];
 
+
         copyToWorkingDirectory();
+        creatingOutput();
 
     }
 
     private static void copyToWorkingDirectory(){
+
         String pageFilesDir = "Project2_test_and_page_files/page_files/";
         String workingDir = "Working_Directory/page_files/";
 
@@ -32,7 +35,7 @@ public class Main {
 
         }
 
-        for(File file : files){
+        for(File file : files) {
             Path source = Paths.get(file.getPath());
             Path dest = Paths.get(workingDir+file.getName());
             List<String> data = new ArrayList<>();
@@ -49,7 +52,37 @@ public class Main {
 
             }
         }
+
     }
+
+    private static void creatingOutput() {
+
+        final String HEADER = "Address,Read/Write,SoftMiss,HardMiss,AHit\n";
+        final String fileName = "Working_Directory/outputs.csv";
+        try {
+            Path destination = Paths.get(fileName);
+            Files.createFile(destination);
+        } catch (IOException e){ }
+
+        writeToCSV(HEADER);
+    }
+
+    public static void writeToCSV(String content){
+        final String fileName = "Working_Directory/outputs.csv";
+
+        try {
+            FileWriter fw = new FileWriter(fileName);
+            fw.append(content);
+            fw.close();
+        } catch (IOException e) {
+            System.out.print("Write Error\n");
+        }
+    }
+
+
+
+
+
 
 
 }
